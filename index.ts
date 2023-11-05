dbf4252f8f8e05a2caf03522ca28372cbec2b0bd
Universios <tonde@universios.com>
2023-11-05 19:22:25 +0100

    changed to bashscript

--- index.ts
+++ index.ts
@@ -1,33 +1,48 @@
 const express = require('express');
 import { exec } from "child_process";
+import { writeFileSync } from 'fs';
 const app = express();
 const port = 80;
 
 app.get('/', (req: any, res: any) => {
   res.send('Hello World! from EC2');
 });
+
 app.get('/triggerBuild', (req: any, res: any) => {
 
   const website = 'judith.ctunnels.com';
   const webdir = `/mnt/static-cdn/clients/clients-mono/sites/${website}`;
-  const image = '265076617171.dkr.ecr.eu-central-1.amazonaws.com/builder-image:latest';  // replace with your image name
+  const image = '265076617171.dkr.ecr.eu-central-1.amazonaws.com/builder-image:latest';
 
-  // const command = `
-  //   sudo docker run --rm ${image} sh -c "cd ${webdir} && sudo npm run build"
-  //   `;
-  const command = `
-  sudo docker run --rm -v ${webdir}:${webdir} ${image} sh -c "cd ${webdir} && npm run build"
-`;
+  const bashScript = `
+        #!/bin/bash
+        sudo docker run --rm -v ${webdir}:${webdir} ${image} sh -c "cd ${webdir} && npm run build"
+    `;
 
-  exec(command, (err, stdout, stderr) => {
+  const scriptPath = `${webdir}/buildScript.sh`;
+
+  // Write the bash script to the target directory
+  writeFileSync(scriptPath, bashScript);
+
+  // Give execution permissions to the script
+  exec(`chmod +x ${scriptPath}`, (err, stdout, stderr) => {
     if (err) {
-      console.error(err);
-      res.status(500).send('Error in build process');
+      console.error(`Error changing permissions: ${err}`);
+      res.status(500).send('Error changing script permissions');
       return;
     }
 
-    console.log(stdout);
-    res.send('Pages published');
+    // Execute the script
+    exec(scriptPath, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error executing script: ${err}`);
+        res.status(500).send('Error in build process');
+        return;
+      }
+
+      console.log(stdout);
+      res.send('Pages published');
+    });
   });
 });
 
